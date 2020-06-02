c4a0d5c5c6b8b6ebeff8006a756205f44875ac27
Henri Joß <henri.joss@hs-furtwangen.de>
2020-06-02 11:23:38 +0200

    added JavaDocs comment

--- src/main/java/de/hfu/App.java
+++ src/main/java/de/hfu/App.java
@@ -4,21 +4,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * Hello world!
- *
- */
 public class App {
-    public static void main(String[] args) throws IOException {
-            // create a BufferedReader using System.in
-            BufferedReader obj = new BufferedReader(new InputStreamReader(System.in));
-            String str;
+    public static void main(String[] args) {
+           printInputsUppercase();
+    }
 
-            System.out.println("Enter lines of text.");
-            System.out.println("Enter 'stop' to quit.");
-            do {
+    /**
+     * Takes in the arguments from the command line and prints them in uppercase.
+     * The function loops as long as the user isn't typing "stop"
+     */
+    public static void printInputsUppercase() {
+        BufferedReader obj = new BufferedReader(new InputStreamReader(System.in));
+        String str = "";
+
+        System.out.println("Enter lines of text.");
+        System.out.println("Enter 'stop' to quit.");
+
+        do {
+            try {
                 str = obj.readLine();
-                System.out.println(str.toUpperCase());
-            } while (!str.equals("stop"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(str.toUpperCase());
+        } while (!str.equals("stop"));
+
     }
 }
